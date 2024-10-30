96fbb76595eba3a940e2a570fc3d6ad0dffad8a6
Roy Mosby <ego.madking@gmail.com>
2024-10-29 20:18:22 -0400

    add inventory, cart, header, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import ctdLogo from './assets/mono-blue-logo.svg'
-import './App.css'
+import { useRef, useState } from 'react';
+import './App.css';
+import './assets/css-reset.css';
+import catalog from './assets/catalog.json';
+import Header from './Header';
+import ProductList from './ProductList';
+import ProductCard from './ProductCard';
 
 function App() {
+  const [inventory, setInventory] = useState(catalog.products);
+  const [cart, setCart] = useState([]);
+  const year = useRef(
+    (() => {
+      const now = new Date(Date.now());
+      return now.getFullYear();
+    })()
+  );
 
+  function promoteProduct() {
+    return (
+      <ProductCard
+        name="Special Limited Edition Tee!"
+        description="Special limited edition neon green shirt with a metallic Code the Dream Logo shinier than the latest front-end framework! Signed by the legendary Frank!"
+      />
+    );
+  }
   return (
-    <div className='coming-soon'>
-    <h1>CTD Swag</h1>
-      <div style={{height:100, width:100}}>
-        <img src={ctdLogo} alt="Code The Dream Logo" />
-      </div>
-      <h2>Coming Soon...</h2>
-    </div>
-  )
+    <>
+      <Header cart={cart} />
+      <main>
+        <ProductList inventory={inventory}>{promoteProduct()}</ProductList>
+      </main>
+      <footer>
+        <p>
+          Made with ❤️ | &copy; {year.current}{' '}
+          <a href="https://codethedream.org/">CTD </a>
+        </p>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
