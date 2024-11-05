229b89297a8314a5329732a28d1865ad8ee8a336
Roy Mosby <ego.madking@gmail.com>
2024-11-05 10:18:41 -0500

    add cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import './assets/css-reset.css';
 import catalog from './assets/catalog.json';
+import Cart from './Cart';
 import Header from './Header';
 import ProductList from './ProductList';
-import ProductCard from './ProductCard';
 
 function App() {
   const [inventory, setInventory] = useState([]);
   const [cart, setCart] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
   const year = useRef(
     (() => {
       const now = new Date(Date.now());
@@ -34,14 +35,29 @@ function App() {
     setCart([...updatedCart]);
   }
 
+  function handleCloseCart() {
+    //prevents re-render if unchanged
+    if (isCartOpen) {
+      setIsCartOpen(false);
+    }
+  }
+
+  function handleOpenCart() {
+    //prevents re-render if unchanged
+    if (!isCartOpen) {
+      setIsCartOpen(true);
+    }
+  }
+
   return (
     <>
-      <Header cart={cart} />
+      <Header cart={cart} handleOpenCart={handleOpenCart} />
       <main>
         <ProductList
           inventory={inventory}
           handleAddItemToCart={handleAddItemToCart}
         ></ProductList>
+        {isCartOpen && <Cart cart={cart} handleCloseCart={handleCloseCart} />}
       </main>
       <footer>
         <p>
