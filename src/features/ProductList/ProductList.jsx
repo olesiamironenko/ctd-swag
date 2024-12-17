b23dace2bbe7a40eb81692136c431c0e08c66b31
Roy Mosby <ego.madking@gmail.com>
2024-12-17 10:17:05 -0500

    reshape inventory to products

--- src/features/ProductList/ProductList.jsx
+++ src/features/ProductList/ProductList.jsx
@@ -1,16 +1,43 @@
+import { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 
 function ProductList({ inventory, handleAddItemToCart }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const workingProducts = [];
+    inventory.forEach((item) => {
+      if (!item.inStock) {
+        return;
+      }
+      if (
+        !workingProducts.find(
+          (productItem) => productItem.baseName === item.baseName
+        )
+      ) {
+        workingProducts.push({
+          baseName: item.baseName,
+          price: item.price,
+          baseDescription: item.baseDescription,
+          variants: [{ ...item }],
+        });
+      } else {
+        const index = workingProducts.findIndex(
+          (productItem) => productItem.baseName === item.baseName
+        );
+        workingProducts[index].variants.push({ ...item });
+      }
+    });
+    setProducts([...workingProducts]);
+  }, [inventory]);
   return (
     <ul className="productList">
-      {inventory.map((product) => {
+      {products.map((product) => {
         return (
           <ProductCard
-            id={product.id}
-            key={product.id}
+            product={product}
+            key={product.baseName}
             handleAddItemToCart={handleAddItemToCart}
-            name={product.name}
-            description={product.description}
           />
         );
       })}
