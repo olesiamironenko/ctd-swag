fb1f6f44cbd7038032b1278afaaba23ca92e75fc
Roy Mosby <ego.madking@gmail.com>
2024-12-27 13:04:30 -0500

    add user and cart state flows for login

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,21 +1,56 @@
+import { useEffect, useState } from 'react';
 import ctdLogo from '../assets/icons/mono-blue-logo.svg';
 import shoppingCart from '../assets/icons/shoppingCart.svg';
 
-function Header({ cart, handleOpenCart }) {
-  function getItemCount() {
-    return cart.reduce((acc, item) => acc + item.itemCount, 0);
-  }
-
+function Header({
+  cart,
+  handleOpenCart,
+  handleOpenAuthForm,
+  user,
+  handleLogOut,
+}) {
+  const [cartCount, setCartCount] = useState(0);
+  useEffect(() => {
+    console.log(cart);
+    if (cart.length > 0) {
+      setCartCount(
+        cart.reduce((acc, item) => {
+          console.log(acc, ' | ', item);
+          return acc + item.quantity;
+        }, 0)
+      );
+    } else {
+      setCartCount(0);
+    }
+  }, [cart, user]);
   return (
     <header>
       <div className="siteBranding">
         <img src={ctdLogo} alt="Code The Dream Logo" />
         <h1>CTD Swag</h1>
       </div>
+      <div className="userActions">
+        {user.id ? (
+          <>
+            <span>Hi, {user.firstName}</span>
+            <button className="authButton signOut" onClick={handleLogOut}>
+              Sign out
+            </button>
+          </>
+        ) : (
+          <button
+            className="authButton"
+            type="button"
+            onClick={handleOpenAuthForm}
+          >
+            Log in
+          </button>
+        )}
+      </div>
       <div className="shoppingCart">
         <button type="button" onClick={handleOpenCart}>
           <img src={shoppingCart} alt="" />
-          <p className="cartCount">{getItemCount()}</p>
+          <p className="cartCount">{cartCount}</p>
         </button>
       </div>
     </header>
