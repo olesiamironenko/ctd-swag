cad223f04a91ae26dff986f068334cda74f6ac7b
Roy Mosby <ego.madking@gmail.com>
2024-12-27 13:02:47 -0500

    add AuthForm, state flows for login user

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import './assets/css-reset.css';
+import AuthForm from './features/Auth/AuthForm';
 import Cart from './features/Cart/Cart';
 import Footer from './layout/Footer';
 import Header from './layout/Header';
 import ProductList from './features/ProductList/ProductList';
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
-
 function App() {
   const [inventory, setInventory] = useState([]);
   const [cart, setCart] = useState([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isAuthFormOpen, setIsAuthFormOpen] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [user, setUser] = useState({});
+  const [authError, setAuthError] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -28,6 +32,33 @@ function App() {
     })();
   }, []);
 
+  async function handleAuthenticate(credentials) {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(credentials),
+      headers: { 'Content-Type': 'application/json' },
+    };
+    try {
+      setIsAuthenticating(true);
+      const resp = await fetch(`${baseUrl}/auth/login`, options);
+      if (!resp.ok) {
+        if (resp.status === 401) {
+          setAuthError('email or password incorrect');
+        }
+        throw new Error(resp.status);
+      }
+      const userData = await resp.json();
+      setUser({ ...userData.user, token: userData.token });
+      setCart([...userData.cartItems]);
+      setAuthError('');
+      setIsAuthenticating(false);
+      setIsAuthFormOpen(false);
+    } catch (error) {
+      setIsAuthenticating(false);
+      console.log(error.message);
+    }
+  }
+
   function handleAddItemToCart(id) {
     const inventoryItem = inventory.find((item) => item.id === id);
     if (!inventoryItem) {
@@ -38,32 +69,43 @@ function App() {
     if (itemToUpdate) {
       updatedCartItem = {
         ...itemToUpdate,
-        itemCount: itemToUpdate.itemCount + 1,
+        quantity: itemToUpdate.quantity + 1,
       };
     } else {
-      updatedCartItem = { ...inventoryItem, itemCount: 1 };
+      updatedCartItem = { ...inventoryItem, quantity: 1 };
     }
     setCart([...cart.filter((item) => item.id !== id), updatedCartItem]);
   }
 
   function handleCloseCart() {
-    //prevents re-render if unchanged
-    if (isCartOpen) {
-      setIsCartOpen(false);
-    }
+    setIsCartOpen(false);
+    setAuthError('');
   }
 
-  function handleOpenCart() {
-    //prevents re-render if unchanged
-    if (!isCartOpen) {
-      setIsCartOpen(true);
-    }
+  function handleLogOut() {
+    setUser({});
+    setCart([]);
   }
 
   return (
     <>
-      <Header cart={cart} handleOpenCart={handleOpenCart} />
+      <Header
+        cart={cart}
+        handleOpenCart={() => setIsCartOpen(true)}
+        handleOpenAuthForm={() => setIsAuthFormOpen(true)}
+        handleLogOut={handleLogOut}
+        user={user}
+      />
       <main>
+        {isAuthFormOpen && (
+          <AuthForm
+            handleCloseAuthForm={() => setIsAuthFormOpen(false)}
+            handleAuthenticate={handleAuthenticate}
+            authError={authError}
+            isAuthenticating={isAuthenticating}
+            resetAuthError={() => setAuthError('')}
+          />
+        )}
         <ProductList
           inventory={inventory}
           handleAddItemToCart={handleAddItemToCart}
