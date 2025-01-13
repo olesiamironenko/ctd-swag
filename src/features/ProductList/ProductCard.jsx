13f931452813d1e98c2d4b7f73b8191122e49699
Roy Mosby <ego.madking@gmail.com>
2025-01-13 14:18:19 -0500

    implement styled-components in ProductCard

--- src/features/ProductList/ProductCard.jsx
+++ src/features/ProductList/ProductCard.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import placeholder from '../../assets/placeholder.png';
 import ProductCardVariants from './ProductCardVariants';
 
 function ProductCard({ product, handleAddItemToCart }) {
   const [areVariantsShown, setAreVariantsShown] = useState(false);
   return (
-    <li className="productCard">
-      <div className="productPreview">
+    <Card>
+      <Preview>
         <img src={placeholder} alt=" " />
-      </div>
-      <div className="productCopy">
+      </Preview>
+      <Copy>
         <h2>{product.baseName}</h2>
-        <p>{product.baseDescription}</p>
-        <p>${product.price.toFixed(2) || '0.00'}</p>
-      </div>
-      <div className="productButtons">
+        <Details>{product.baseDescription}</Details>
+        <Details>${product.price.toFixed(2) || '0.00'}</Details>
+      </Copy>
+      <ButtonWrapper>
         {product.variants.length > 1 ? (
           <button onClick={() => setAreVariantsShown(true)}>
             Show Options
@@ -24,7 +25,7 @@ function ProductCard({ product, handleAddItemToCart }) {
             Add to Cart
           </button>
         )}
-      </div>
+      </ButtonWrapper>
       {areVariantsShown && (
         <ProductCardVariants
           handleAddItemToCart={handleAddItemToCart}
@@ -32,8 +33,49 @@ function ProductCard({ product, handleAddItemToCart }) {
           closeVariants={() => setAreVariantsShown(false)}
         />
       )}
-    </li>
+    </Card>
   );
 }
+
+const Card = styled.li`
+  position: relative;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 200px;
+  border-radius: var(--medium-radius);
+  justify-content: space-between;
+`;
+
+const Preview = styled.div`
+  border-radius: var(--medium-radius) var(--medium-radius) 0 0;
+  overflow: hidden;
+  border: 4px solid var(--dark-blue);
+`;
+
+const Copy = styled.div`
+  padding: 0.25rem;
+`;
+const Details = styled.p`
+  padding: 0.5rem;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  button {
+    border: none;
+    background-color: rgb(from var(--medium-blue) r g b / 0.5);
+    width: 100%;
+    &:hover {
+      background-color: rgb(from var(--medium-blue) r g b / 0.25);
+    }
+    &:active {
+      background-color: rgb(from var(--light-blue) r g b / 0.25);
+    }
+  }
+`;
 
 export default ProductCard;
