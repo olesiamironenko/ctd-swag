a71a19f324b9ce623586c44db411cb13518d8775
Roy Mosby <ego.madking@gmail.com>
2025-02-02 21:24:32 -0500

    move conversion of inventory to App

--- src/features/ProductList/ProductList.jsx
+++ src/features/ProductList/ProductList.jsx
@@ -1,35 +1,6 @@
-import { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 
-function ProductList({ inventory, handleAddItemToCart }) {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const workingProducts = [];
-    inventory.forEach((item) => {
-      if (!item.inStock) {
-        return;
-      }
-      if (
-        !workingProducts.find(
-          (productItem) => productItem.baseName === item.baseName
-        )
-      ) {
-        workingProducts.push({
-          baseName: item.baseName,
-          price: item.price,
-          baseDescription: item.baseDescription,
-          variants: [{ ...item }],
-        });
-      } else {
-        const index = workingProducts.findIndex(
-          (productItem) => productItem.baseName === item.baseName
-        );
-        workingProducts[index].variants.push({ ...item });
-      }
-    });
-    setProducts([...workingProducts]);
-  }, [inventory]);
+function ProductList({ products, handleAddItemToCart }) {
   return (
     <ul className="productList">
       {products.map((product) => {
