0cb8c1466b5fec32da31233889552339e973431a
Roy Mosby <ego.madking@gmail.com>
2025-02-02 21:25:56 -0500

    extract conversion util function from ProductList

---
+++ src/utils/convertInventoryToProducts.js
@@ -0,0 +1,27 @@
+export function convertInventoryToProducts(inventory) {
+  console.log(inventory);
+  const workingProducts = [];
+  inventory.forEach((item) => {
+    if (!item.inStock) {
+      return;
+    }
+    if (
+      !workingProducts.find(
+        (productItem) => productItem.baseName === item.baseName
+      )
+    ) {
+      workingProducts.push({
+        baseName: item.baseName,
+        price: item.price,
+        baseDescription: item.baseDescription,
+        variants: [{ ...item }],
+      });
+    } else {
+      const index = workingProducts.findIndex(
+        (productItem) => productItem.baseName === item.baseName
+      );
+      workingProducts[index].variants.push({ ...item });
+    }
+  });
+  return [...workingProducts];
+}
