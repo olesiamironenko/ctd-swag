5ee862e22da6b847422f957705856d8234a3bb23
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-20 08:53:59 -0400

    App.jsx in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
 import ctdLogo from './assets/icons/mono-blue-logo.svg';
-import inventoryData from './assets/inventory.json';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-  const [inventory, setInventory] = useState(inventoryData.inventory);
-
   return (
-    <main>
-      <div className="coming-soon">
-        <h1>CTD Swag</h1>
-        <div style={{ height: 100, width: 100 }}>
-          <img src={ctdLogo} alt="Code The Dream Logo" />
-        </div>
+    <div className="coming-soon">
+      <h1>CTD Swag</h1>
+      <div style={{ height: 100, width: 100 }}>
+        <img src={ctdLogo} alt="Code The Dream Logo" />
       </div>
-      <ul>
-        {inventory.map((item) => {
-          return (
-            <li key={item.id}>
-              <div className="itemCard">
-                <h2>{item.baseName}</h2>
-                <p>{item.baseDescription}</p>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </main>
+      <h2>Coming Soon...</h2>
+    </div>
   );
 }
 
--- src/pages/404/404.jsx
+++
@@ -1,4 +0,0 @@
-function NotFound() {
-  return <h2>404: Not Found</h2>;
-}
-export default NotFound;
