f07c169e4fc6df3ea0aec4ca8724ba3041d39359
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-20 20:31:57 -0400

    logo centered

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="coming-soon">
       <h1>CTD Swag</h1>
-      <div style={{ height: 100, width: 100 }}>
-        <img src={ctdLogo} alt="Code The Dream Logo" />
+      <div>
+        <img
+          src={ctdLogo}
+          alt="Code The Dream Logo"
+          style={{ height: 100, width: 100 }}
+        />
       </div>
       <h2>Coming Soon...</h2>
     </div>
