b4243c43b660e0c0ba5ba2519b0e78e73b2ee154
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-21 07:42:02 -0400

    main branch reset

--- src/utils/sortByPrice.js
+++
@@ -1,9 +0,0 @@
-export function sortByPrice({ productItems, isSortAscending }) {
-  return productItems.toSorted((a, b) => {
-    if (isSortAscending) {
-      return a.price - b.price;
-    } else {
-      return b.price - a.price;
-    }
-  });
-}
