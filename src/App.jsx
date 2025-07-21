528b9ac96e20403e29df481df12cab5a615fb17d
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-21 07:36:14 -0400

    components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import ctdLogo from './assets/icons/mono-blue-logo.svg';
 import './App.css';
 
+function Component1() {
+  return <p>Component1 uses a function declaration</p>;
+}
+
+//or
+
+const Component2 = function () {
+  return <p>Component2 uses a function expression</p>;
+};
+
+//or
+
+const Component3 = () => {
+  return <p>Component3 uses an arrow function</p>;
+};
+
+//or
+
+const Component4 = () => (
+  <p>Component4 uses an arrow function with implicit return</p>
+);
+
 function App() {
   return React.createElement(
     'div',
@@ -19,7 +41,14 @@ function App() {
         alt: 'Code The Dream Logo',
       })
     ),
+    // Inserted components
+    React.createElement(Component1), // using function declaration
+    React.createElement(Component2), // using function expression
+    React.createElement(Component3), // arrow function
+    React.createElement(Component4), // arrow function with implicit return
+
     React.createElement('h2', null, 'Coming Soon...')
   );
 }
+
 export default App;
