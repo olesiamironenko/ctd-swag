ac18fe6a32bb3ed06ffbec9071026da1000f9734
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-21 07:13:12 -0400

    styles connected, app in react, logo centered

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import React from 'react';
 import ctdLogo from './assets/icons/mono-blue-logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="coming-soon">
-      <h1>CTD Swag</h1>
-      <div>
-        <img
-          src={ctdLogo}
-          alt="Code The Dream Logo"
-          style={{ height: 100, width: 100 }}
-        />
-      </div>
-      <h2>Coming Soon...</h2>
-    </div>
+  return React.createElement(
+    'div',
+    {
+      className: 'coming-soon',
+    },
+    React.createElement('h1', null, 'CTD Swag'),
+    React.createElement(
+      'div',
+      {
+        className: 'siteBranding',
+      },
+      React.createElement('img', {
+        src: ctdLogo,
+        alt: 'Code The Dream Logo',
+      })
+    ),
+    React.createElement('h2', null, 'Coming Soon...')
   );
 }
-
 export default App;
