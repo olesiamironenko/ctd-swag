3e2d81921c2a575336e4737d5c57f63513723e9a
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-21 22:23:27 -0400

    ProductCard.jsx is added

---
+++ src/ProductCard.jsx
@@ -0,0 +1,14 @@
+/*ProductCard.jsx*/
+
+function ProductCard({ baseName, baseDescription }) {
+  return (
+    <li>
+      <div className="itemCard">
+        <h2>{props.baseName}</h2>
+        <p>{props.baseDescription}</p>
+      </div>
+    </li>
+  );
+}
+
+export default ItemCard;
