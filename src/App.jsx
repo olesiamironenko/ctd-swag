77f275d10393d0e74e89833e81cc097548f8f598
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-21 22:07:36 -0400

    inventory list component added to the App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import ctdLogo from './assets/icons/mono-blue-logo.svg';
 import './App.css';
 import inventoryData from './assets/inventory.json';
 
+//without destructuring
+function ProductList(props) {
+  const inventory = props.inventory;
+  return (
+    <ul>
+      {inventory.map((item) => {
+        return <li key={item.id}>{item.baseName}</li>;
+      })}
+    </ul>
+  );
+}
+
 function App() {
   const [inventory, setInventory] = useState(inventoryData.inventory);
   return (
@@ -17,18 +29,7 @@ function App() {
           />
         </div>
       </div>
-      <ul>
-        {inventory.map((item) => {
-          return (
-            <li key={item.id}>
-              <div className="itemCard">
-                <h2>{item.baseName}</h2>
-                <p>{item.baseDescription}</p>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      <ProductList inventory={inventory} />
     </main>
   );
 }
