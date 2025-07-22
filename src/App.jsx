174f1a2077d36bb00c2a5f525b4997ee884a0136
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-22 09:00:41 -0400

    PromoteItem and children added to ProductList

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import React, { useState } from 'react';
 import Header from './Header.jsx';
 import inventoryData from './assets/inventory.json';
 import ProductList from './ProductList';
+import ProductCard from './ProductCard';
 import './App.css';
 
 function App() {
   const [inventory, setInventory] = useState(inventoryData.inventory);
 
+  function promoteItem() {
+    return (
+      <ProductCard
+        baseName="Limited Edition Tee!"
+        baseDescription="Special limited edition neon green shirt with a metallic Code the Dream Logo shinier than the latest front-end framework! Signed by the legendary Frank!"
+      />
+    );
+  }
+
   return (
     <main>
       <Header />
-      <ProductList inventory={inventory} />
+      <ProductList inventory={inventory}>{promoteItem()}</ProductList>
     </main>
   );
 }
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,13 @@
 import ProductCard from './ProductCard';
 
 //with destructuring
-function ProductList({ inventory = [] }) {
+function ProductList({ inventory = [], children }) {
   //destructuring assignment grabs `inventory` out of props
   //we're also setting a default value og `inventory` to an empty array
   return (
     <ul>
+      {children}{' '}
+      {/* this location guarantees that this list item will be first */}
       {inventory.map((item) => {
         return (
           <ProductCard
