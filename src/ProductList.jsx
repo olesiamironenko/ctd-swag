93ac9390fc5cc6f85a48e06feec1d5532d256dea
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-28 14:14:30 -0400

    useEffect added to ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from 'react';
 import ProductCard from './ProductCard';
 
 //with destructuring
 function ProductList({ inventory = [], children, handleAddItemToCart }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const workingProducts = [];
+    inventory.forEach((item) => {
+      if (!item.inStock) {
+        return;
+      }
+      if (
+        !workingProducts.find(
+          (productItem) => productItem.baseName === item.baseName
+        )
+      ) {
+        workingProducts.push({
+          baseName: item.baseName,
+          price: item.price,
+          baseDescription: item.baseDescription,
+          variants: [{ ...item }],
+        });
+      } else {
+        const index = workingProducts.findIndex(
+          (productItem) => productItem.baseName === item.baseName
+        );
+        workingProducts[index].variants.push({ ...item });
+      }
+    });
+    setProducts([...workingProducts]);
+  }, [inventory]);
+
   //destructuring assignment grabs `inventory` out of props
   //we're also setting a default value og `inventory` to an empty array
   return (
