89502b56df2b8d1598ab9765bca6a58d86546f1c
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-29 08:47:45 -0400

    emppty cart message added

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -7,19 +7,23 @@ function Cart({ cart, handleCloseCart }) {
       <div className="cartScreen"></div>
       {/* .cartScreen covers the product list with a div that has a blur effect placed on it. this makes the product buttons unclickable */}
       <div className="cartListWrapper">
-        <ul className="cartList">
-          {cart.map((item) => {
-            return (
-              <li className="cartListItem" key={item.cartItemId}>
-                <img src={placeholder} alt="" />
-                <h2>{item.baseName}</h2>
-                <div className="cartListItemSubtotal">
-                  <p>${item.price}</p>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        {cart.length === 0 ? (
+          <p>cart is empty</p>
+        ) : (
+          <ul className="cartList">
+            {cart.map((item) => {
+              return (
+                <li className="cartListItem" key={item.cartItemId}>
+                  <img src={placeholder} alt="" />
+                  <h2>{item.baseName}</h2>
+                  <div className="cartListItemSubtotal">
+                    <p>${item.price}</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        )}
         {/* cart total will need to be calculated */}
         <h2>Cart Total: $0.00</h2>
         <button onClick={handleCloseCart}>CloseCart</button>
